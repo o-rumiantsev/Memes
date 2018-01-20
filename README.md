d8442f3ac35104b7e2730de0ded28633fced050d
o-rumiantsev <alex.rum12.12.12@gmail.com>
2018-01-20 22:21:07 +0200

    Implemetned getting two memes from db by session id

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Memes
-Memes Time backend
+Memes Time
--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-'use strcit';
+'use strict';
 
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Here will be our Meme Time');
+});
+
+app.listen(3000, '0.0.0.0');
