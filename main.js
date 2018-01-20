1b55abb081d3499a7a22db1f311e8e88b2c36dcb
o-rumiantsev <alex.rum12.12.12@gmail.com>
2018-01-21 00:28:23 +0200

    Minor improvement in main.js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 'use strict';
 
 const express = require('express');
+const getMemesFromDb = require('./lib/getMemesFromDb.js');
+// const recieveMemesFromUser = require('./lib/recieveMemesFromUser.js');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Here will be our Meme Time');
+});
+
+app.get('/getMemes', (req, res) => {
+  const sessionId = req.sessionId;
+  if (!req.data) getMemesFromDb(sessionId, (err, memes) => {
+    if (err) res.send(err);
+    else res.send(memes);
+  });
+  // else recieveMemesFromUser(req);
 });
 
 app.listen(3000, '0.0.0.0');
