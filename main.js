11a77fc65eb6823f0f8ab59a169a28eca9b28efd
o-rumiantsev <alex.rum12.12.12@gmail.com>
2018-01-22 17:27:32 +0200

    Improved recievememesFromUser.js

--- main.js
+++ main.js
@@ -1,23 +1,45 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const getMemesFromDb = require('./lib/getMemesFromDb.js');
 const recieveMemesFromUser = require('./lib/recieveMemesFromUser.js');
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
   res.send('Here will be our Meme Time');
 });
 
+
 app.post('/getMemes', (req, res) => {
+  console.log(req.body);
   const sessionId = req.body.sessionId;
-  if (!req.data) getMemesFromDb(sessionId, (err, memes) => {
+  recieveMemesFromUser(req, (err) => {
     if (err) res.send(err);
-    else res.send(memes);
-  });
-  else recieveMemesFromUser(req, (err) => {
-    if (err) res.send(err);
+    else sendMemes(res, sessionId);
   });
 });
 
 app.listen(3000, '0.0.0.0');
+
+function sendMemes(res, sessionId) {
+  getMemesFromDb(sessionId, (err, memes) => {
+    if (err) res.status(500).send(err).end();
+    else {
+      const frontMemes = memes.map(
+        (meme) => ({
+          _id: meme._id,
+          url: meme.url
+        })
+      );
+      res.json({ frontMemes }).end();
+    }
+  });
+}
