08ecc021e4d9fc16c925efcb86c0171d5eebf2f1
o-rumiantsev <alex.rum12.12.12@gmail.com>
2018-01-22 14:29:50 +0200

    Implemented recieving memes from users add method 'getMeme' to mongo.js refactored rating.js changed app.get() to app.post() in main.js

--- main.js
+++ main.js
@@ -2,20 +2,22 @@
 
 const express = require('express');
 const getMemesFromDb = require('./lib/getMemesFromDb.js');
-// const recieveMemesFromUser = require('./lib/recieveMemesFromUser.js');
+const recieveMemesFromUser = require('./lib/recieveMemesFromUser.js');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Here will be our Meme Time');
 });
 
-app.get('/getMemes', (req, res) => {
-  const sessionId = req.sessionId;
+app.post('/getMemes', (req, res) => {
+  const sessionId = req.body.sessionId;
   if (!req.data) getMemesFromDb(sessionId, (err, memes) => {
     if (err) res.send(err);
     else res.send(memes);
   });
-  // else recieveMemesFromUser(req);
+  else recieveMemesFromUser(req, (err) => {
+    if (err) res.send(err);
+  });
 });
 
 app.listen(3000, '0.0.0.0');
