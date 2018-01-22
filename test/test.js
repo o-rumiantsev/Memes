e2e2daebac417684a38a0707c15329eb242f2e3d
o-rumiantsev <alex.rum12.12.12@gmail.com>
2018-01-23 00:26:18 +0200

    Implemented test for app.post()

--- test/test.js
+++ test/test.js
@@ -1,9 +1,42 @@
 'use strict';
 
 const getMemesFromDb = require('../lib/getMemesFromDb.js');
-const sessionId = 12345;
+const recieveMemesFromUser = require('../lib/recieveMemesFromUser.js');
 
-getMemesFromDb(sessionId, (err, memes) => {
-  if (err) throw err;
-  console.log(memes);
-});
+const req = {
+  body: {
+    sessionId: '12345',
+    data: [
+      { _id: '5a6656c88a9c534d590b2f2a', clicked: true },
+      { _id: '5a6656c88a9c534d590b2f26', clicked: false }
+    ]
+  }
+};
+
+const sendMemes = (sessionId) => {
+  getMemesFromDb(sessionId, (err, memes) => {
+    if (err) {
+      console.error(err);
+      // res.status(500).end();
+    } else {
+      const frontMemes = memes.map(
+        (meme) => ({
+          _id: meme._id,
+          url: meme.url
+        })
+      );
+      console.log(frontMemes);
+      // res.json({ frontMemes }).end();
+    }
+  });
+};
+
+const appPost = (req) => {
+  const sessionId = req.body.sessionId;
+  recieveMemesFromUser(req, (err) => {
+    if (err) console.error(err);
+    else sendMemes(sessionId);
+  });
+};
+
+appPost(req);
